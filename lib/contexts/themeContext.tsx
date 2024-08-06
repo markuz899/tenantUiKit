d812edb713f2828889fb5400cfe039c5631998cb
markuz899 <markuz.89@hotmail.it>
2024-08-06 14:25:14 +0200

    [feat] - theme provider

---
+++ lib/contexts/themeContext.tsx
@@ -0,0 +1,47 @@
+// import { ReactNode, createContext, useContext, useState } from "react";
+
+// interface ThemeContextType {
+//   theme: any;
+//   toggleTheme: () => void;
+// }
+
+// interface IProps {
+//   theme?: any;
+//   children: ReactNode;
+// }
+
+// export const ThemeContext = createContext<ThemeContextType | null>(null);
+
+// const ThemeContextProvider = ({ theme, children }: IProps) => {
+//   const [currentTheme, setTheme] = useState(theme);
+
+//   return (
+//     <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+//   );
+// };
+
+// export default ThemeContextProvider;
+
+// export const useTheme = () => {
+//   const context = useContext(ThemeContext);
+//   if (context === null) {
+//     throw new Error("Context must be used within a context provider");
+//   }
+//   return context;
+// };
+
+interface ThemeProviderProps {
+  theme?: any;
+  children: any;
+}
+
+import styled, {
+  css,
+  ThemeProvider as StyledProvider,
+} from "styled-components";
+
+const ThemeProvider = ({ theme, children }: ThemeProviderProps) => {
+  return <StyledProvider theme={theme}>{children}</StyledProvider>;
+};
+
+export { ThemeProvider, styled, css };
--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,9 @@
-import { Button } from "./components";
 import "./index.css";
+// components
+import { Button } from "./components";
+import { ThemeProvider, styled, css } from "./contexts/themeContext";
+// context
+// import ThemeContext, { useTheme } from "./contexts/themeContext";
 
-export { Button };
+// export { Button, ThemeContext, useTheme };
+export { Button, ThemeProvider, styled, css };
