e10d9e8cb15333570aae9f0b8af1ba82a6297eca
markuz899 <markuz.89@hotmail.it>
2024-09-07 12:31:46 +0200

    hooks

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,22 +1,22 @@
 import { useTheme } from "../../hooks/useTheme";
 import { ButtonProps } from "./interface";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-interface Theme {
-  colors: {
-    success: string;
-  };
-}
+// interface Theme {
+//   colors: {
+//     success: string;
+//   };
+// }
 
-const Style = styled.button<{ theme: Theme }>`
-  background: ${({ theme }) => theme.colors.success};
-`;
+// const Style = styled.button<{ theme: Theme }>`
+//   background: ${({ theme }) => theme.colors.success};
+// `;
 
 const Button = ({ children, label }: ButtonProps) => {
   const { themeCurrent } = useTheme();
   console.log("from comp", themeCurrent);
   const content = children || label;
-  return <Style>{content}</Style>;
+  return <button>{content}</button>;
 };
 
 export default Button;
